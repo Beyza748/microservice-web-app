86a66a8d1b4d31135d7801a8aa90f207326f784a
Beyza748 <kahramanbeyza2022@gmail.com>
2025-12-25 12:49:17 +0300

    Update app.py

--- product-service/app.py
+++ product-service/app.py
@@ -7,6 +7,3 @@ app = Flask(__name__)
 def products():
     data = requests.get("https://dummyjson.com/products?limit=5").json()
     return jsonify(data["products"])
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
