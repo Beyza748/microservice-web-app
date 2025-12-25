dd8c296b253bc658820e7d5fc89b760c7a3a034d
Beyza748 <kahramanbeyza2022@gmail.com>
2025-12-25 12:50:33 +0300

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -8,6 +8,3 @@ def index():
     products = requests.get("http://product-service:5001/products").json()
     holidays = requests.get("http://holiday-service:5002/holidays").json()
     return render_template("index.html", products=products, holidays=holidays)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
