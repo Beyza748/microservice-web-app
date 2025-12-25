1ad6a57bc05d5f44ec95a15dcc02b3eaf806b14b
Beyza748 <kahramanbeyza2022@gmail.com>
2025-12-25 12:49:52 +0300

    Update app.py

--- holiday-service/app.py
+++ holiday-service/app.py
@@ -7,6 +7,3 @@ app = Flask(__name__)
 def holidays():
     url = "https://date.nager.at/api/v3/PublicHolidays/2025/TR"
     return jsonify(requests.get(url).json()[:5])
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002)
